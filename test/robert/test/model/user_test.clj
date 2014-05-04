6c166459040bd229b377f21ddffe2b8ebb560a04
Simone "siscia" Mosciatti <simone@mweb.biz>
2014-05-04 20:28:22 +0200

    add first tests

---
+++ test/robert/test/model/user_test.clj
@@ -0,0 +1,16 @@
+(ns robert.test.model.user-test
+  (:require [monger.core :refer [connect! get-db set-db!]]
+            [monger.collection :as mc]
+            
+            [monger.test.manage-db :as manage]
+            [monger.data.data :as data]))
+
+(connect!)
+(set-db! (get-db (:name data/main-document)))
+
+(manage/eliminate-document (:name data/main-document))
+(manage/create-document (:name data/main-document))
+(manage/create-first-users (:name data/main-document))
+
+
+
