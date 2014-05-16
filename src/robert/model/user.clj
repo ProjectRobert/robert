77b8fd4d0b0f68a345c24b07bfc4b60d1a01f558
Simone "siscia" Mosciatti <simone@mweb.biz>
2014-05-17 01:43:50 +0200

    brute force migration

--- src/robert/model/user.clj
+++ src/robert/model/user.clj
@@ -1,15 +1,13 @@
 (ns robert.model.user
-  (:require [monger.core :refer [connect! set-db! get-db]]
+  (:require [monger.core :refer [get-db]]
             [monger.collection :as mc]
             [monger.joda-time]
             [monger.operators :refer :all]
             [cemerick.friend.credentials :as creds]
             [noir.validation :as v]
             [clj-time.core :as t])
-  (:require [robert.utils :as u]))
-
-(connect!)
-(set-db! (get-db "robert"))
+  (:require [robert.utils :as u]
+            [robert.model.config :refer [connection]]))
 
 (alter-var-root (var v/*errors*) (fn [_] (atom {})))
 
@@ -22,80 +20,80 @@
        :new_requested_email :email_change_code :email_change_code_created_at
        :created_at :updated_at])
 
-(defn- unique? [collection {:keys [record value] :as map-values}]
-  (if-not (mc/find-one-as-map collection map-values)
+(defn- unique? [database {:keys [record value] :as map-values}]
+  (if-not (mc/find-one-as-map (get-db connection database) "users" map-values)
     true
     false))
 
-(defn- find-user-login [collection query]
-  (mc/find-one-as-map collection query))
+(defn- find-user-login [database query]
+  (mc/find-one-as-map (get-db connection database) "users" query))
 
-(defn- find [collection query]
-  (mc/find-maps collection query))
+(defn- find [database query]
+  (mc/find-maps (get-db connection database) "users" query))
 
-(defn fetch [collection query & {:keys [multiple] :or [multiple false]}]
+(defn fetch [database query & {:keys [multiple] :or [multiple false]}]
   (if multiple
-    (mc/find-maps collection query)
-    (mc/find-one-as-map collection query)))
+    (mc/find-maps (get-db connection database) "users" query)
+    (mc/find-one-as-map (get-db connection database) "users" query)))
 
-(defn- new-secure-code [collection]
-  (first (filter #(unique? collection {$or [{:activation_code %}
+(defn- new-secure-code [database]
+  (first (filter #(unique? (get-db connection database) "users" {$or [{:activation_code %}
                                             {:email_change_code %}
                                             {:password_reset_code %}]})
                  (repeatedly uuid))))
 
-(defn valid? [collection user-data]
+(defn valid? [database user-data]
   (and (boolean (get user-data "password"))
        (let [email (get user-data "email")
              username (get user-data "username")]
          (cond
-          (and email username) (and (unique? collection {:email email})
-                                    (unique? collection {:username username}))
-          (boolean email) (unique? collection {:email email})
-          (boolean username) (unique? collection {:username username})
+          (and email username) (and (unique? database {:email email})
+                                    (unique? database {:username username}))
+          (boolean email) (unique? database {:email email})
+          (boolean username) (unique? database {:username username})
           :else false))))
 
-(defn user-login [collection creds-map]
+(defn user-login [database creds-map]
   (println "attemp login => " creds-map)
-  (when-let [user (find-user-login collection {$or
+  (when-let [user (find-user-login database {$or
                                                [{:username (:username creds-map)}
                                                 {:email (:email creds-map)}]})]
     user))
 
-(defn ask-change-email! [collection email new-email password]
-  (let [user (mc/find-one-as-map collection {:email email})]
+(defn ask-change-email! [database email new-email password]
+  (let [user (mc/find-one-as-map (get-db connection database) "users" {:email email})]
     (if (creds/bcrypt-verify password (:password user))
-      (mc/save-and-return collection
+      (mc/save-and-return (get-db connection database) "users"
                           (merge user
                                  {:email_change_code (new-secure-code)
                                   :email_change_code_created_at u/now
                                   :new_requested_email new-email}))
       (v/set-error :change-email :wrong-password))))
 
-(defn change-email! [collection reset-code]
-   (if-let [user (mc/find-one-as-map collection {:email_change_code
+(defn change-email! [database reset-code]
+   (if-let [user (mc/find-one-as-map (get-db connection database) "users" {:email_change_code
                                                 reset-code})]
-     (mc/update-by-id collection (:_id user)
+     (mc/update-by-id (get-db connection database) "users" (:_id user)
                {$set {:email (:new_requested_email user)}
                 $unset  {:email_change_code 1
                           :email_change_code_created_at 1
                           :new_requested_email 1}})
      (v/set-error :code :not-valid-email-code)))
 
-(defn ask-change-password! [collection email new-password password]
-   (let [user (mc/find-one-as-map collection {:email email})]
+(defn ask-change-password! [database email new-password password]
+   (let [user (mc/find-one-as-map (get-db connection database) "users" {:email email})]
      (if (creds/bcrypt-verify password (:password user))
-       (mc/save-and-return collection
+       (mc/save-and-return (get-db connection database) "users"
                            (merge user
                                   {:password_reset_code (new-secure-code)
                                    :password_reset_code_created_at u/now
                                    :new_password_requested (creds/hash-bcrypt new-password)}))
       (v/set-error :change-password :wrong-password))))
 
-(defn change-password! [collection reset-code]
-  (if-let [user (mc/find-one-as-map collection {:password_reset_code
+(defn change-password! [database reset-code]
+  (if-let [user (mc/find-one-as-map (get-db connection database) "users" {:password_reset_code
                                               reset-code})]
-    (mc/update-by-id collection (:_id user)
+    (mc/update-by-id (get-db connection database) "users" (:_id user)
                {$set {:password (:new_password_requested user)}
                 $unset {:password_reset_code 1
                          :password_reset_code_created_at 1
@@ -105,34 +103,34 @@
 
 ;; name and email, if the exist they need to be unique in the document
 
-(defn new! [collection user-data]
-  (assert (valid? collection user-data))
+(defn new! [database user-data]
+  (assert (valid? database user-data))
   (let [email (get user-data "email")
         name (get user-data "username")
         password (get user-data "password")]
 
-    (let [activaction-code (new-secure-code collection)
-          user (mc/insert-and-return collection
+    (let [activaction-code (new-secure-code database)
+          user (mc/insert-and-return (get-db connection database) "users"
                                      (assoc user-data
                                        :password (creds/hash-bcrypt password)))]
       (dissoc user :password))))
 
-(defn verify-email! [collection code]
-  (if-let [user (mc/find-one-as-map collection {:activation_code code})]
-    (mc/update-by-id collection (:_id user) {$unset {:activation_code 1
+(defn verify-email! [database code]
+  (if-let [user (mc/find-one-as-map (get-db connection database) "users" {:activation_code code})]
+    (mc/update-by-id (get-db connection database) "users" (:_id user) {$unset {:activation_code 1
                                                      :activation_code_created_at 1}})
     (v/set-error :code :not-valid-validation-code)))
 
-(defn remove! [collection email]
-  (mc/remove collection {:email email}))
+(defn remove! [database email]
+  (mc/remove (get-db connection database) "users" {:email email}))
 
-(defn login [collection creds-map]
-  (println "model.user/login - collection => " collection)
+(defn login [database creds-map]
+  (println "model.user/login - database => " database)
   (println "model.user/login - creds-map => " creds-map)
   (if (and (= (:username creds-map) "test")
            (= (:password creds-map) "test"))
-    {:email "test" :collection "test"}
-    (when-let [user (fetch collection {$or
+    {:email "test" :database "test"}
+    (when-let [user (fetch database {$or
                                        [{:username (get creds-map "username")}
                                         {:email (get creds-map "email")}]})]
       (println "model.user/login - user => " user)
@@ -140,11 +138,11 @@
         (do (println "ok")
             (dissoc user :password))))))
 
-(defn add-last-login [collection user]
-  (mc/update-by-id collection (:_id user) {$set {:last-login (t/now)}}))
+(defn add-last-login [database user]
+  (mc/update-by-id (get-db connection database) "users" (:_id user) {$set {:last-login (t/now)}}))
 
 (defn update-document
-  ([collection old-document delta]
-     (update-document collection (merge old-document delta)))
-  ([collection new-document]
-     (mc/save-and-return collection new-document)))
+  ([database old-document delta]
+     (update-document database (merge old-document delta)))
+  ([database new-document]
+     (mc/save-and-return (get-db connection database) "users" new-document)))
